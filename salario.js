8e7e2acd6433a88d716eb6bb907ddac0653993bd
patrickkjr <pato.games666@gmail.com>
2024-07-12 20:38:09 -0300

    salario

--- salario.js
+++ salario.js
@@ -4,7 +4,19 @@ const banco = require("./banco")
 var salario = banco.conexao.define(
     "salario",
     {
-        
+        id:{
+            type:sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        funcao:{
+            type:sequelize.STRING,
+            allowNull: false
+        },
+        valor:{
+            type:sequelize.DOUBLE,
+            
+        }
     },
     {timestamps: false}
 );
